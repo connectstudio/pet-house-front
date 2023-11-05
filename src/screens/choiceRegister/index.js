0ce9d2a0523b30a4247134dc623d897f1fe92359
Fernanda Bonfim <82246157+fernandabonfimm@users.noreply.github.com>
2023-11-05 16:16:33 -0300

    exemplo de uso card

--- src/screens/choiceRegister/index.js
+++ src/screens/choiceRegister/index.js
@@ -7,23 +7,35 @@ import { FontAwesome5 } from "@expo/vector-icons";
 import { MaterialIcons } from "@expo/vector-icons";
 
 export default function ChoiceRegister() {
+  const arrCards = [
+    {
+      icon: <FontAwesome5 name="house-user" size={24} color="black" />,
+      title: "Cuidador",
+      description:
+        "Crie um cadastro como cuidador e seja um guardião para os nossos anfitriões",
+      onPress: "",
+    },
+    {
+      icon: <MaterialIcons name="pets" size={24} color="black" />,
+      title: "Anfitrião",
+      description:
+        "Crie um cadastro como anfitrião e encontre um guardião para o seu pet",
+      onPress: "",
+    },
+  ];
   return (
     <SafeAreaView>
       <View style={styles.cardsView}>
-        <Card
-          icon={<FontAwesome5 name="house-user" size={24} color="black" />}
-          title={"Cuidador"}
-          description={
-            "Crie um cadastro como cuidador e seja um guardião para os nossos anfitriões"
-          }
-        />
-        <Card
-          icon={<MaterialIcons name="pets" size={24} color="black" />}
-          title={"Anfitrião"}
-          description={
-            "Crie um cadastro como anfitrião e procure um guardião para cuidar do seu pet"
-          }
-        />
+        {arrCards.map((item) => {
+          return (
+            <Card
+              icon={item.icon}
+              title={item.title}
+              description={item.description}
+              onPress={item.onPress}
+            />
+          );
+        })}
       </View>
     </SafeAreaView>
   );
